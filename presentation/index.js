ed626b321e6a7f4002a898d81ad9b2c214c2fe9c
Anton Nikolenko <nikolenkoanton92@gmail.com>
2017-01-26 00:32:08 -0800

    cleanup, add images for last slide

--- presentation/index.js
+++ presentation/index.js
@@ -36,7 +36,9 @@ const images = {
   kat: require("../assets/kat.png"),
   es6Logo: require("../assets/es6-logo.png"),
   startWars: require("../assets/star-wars.jpg"),
-  browserSupport: require("../assets/es6-support.png")
+  browserSupport: require("../assets/es6-support.png"),
+  finish: require("../assets/finish.jpg"),
+  callbacks: require("../assets/callbacks.png")
 };
 
 preloader(images);
@@ -78,32 +80,36 @@ export default class Presentation extends React.Component {
               </Heading>
             </Appear>
           </Slide>
-          <Slide bgColor="#000000">
+          <Slide bgColor="#000000" transition={["spin"]}>
             <Heading size="2" textColor="#ffffff">History</Heading>
             <Image src={images.startWars.replace("/", "")} height="440px" margin="40px 0px 0px 0px" />
           </Slide>
-          <Slide bgColor="#ffffff">
+          <Slide bgColor="#ffffff" transition={["zoom"]}>
             <Layout textColor="#000000">
               <Fill>
                 <List>
                 <Appear><ListItem>May - Mocha is invented in Netscape By Brendan Eich</ListItem></Appear>
                 <Appear><ListItem>September - Renamed to LiveScript</ListItem></Appear>
                 <Appear><ListItem>December - Renamed to Javascript (Because Java was popular)</ListItem></Appear>
-                <Appear><ListItem>1996 - JavaScript is taken to standardization in ECMA.</ListItem></Appear>
-                <Appear><ListItem>1997 - ECMA-262 (ECMAScript)</ListItem></Appear>
-                <Appear><ListItem>1998 - ECMAScript 2</ListItem></Appear>
-                <Appear><ListItem>1999 - ECMAScript 3</ListItem></Appear>
                 </List>
               </Fill>
               <Fill>
-              <List>
-                <Appear><ListItem>2005 - Mozilla and Macromedia started Work on ECMAScript 4, but they renamed to ECMAScript 3.1</ListItem></Appear>
-                <Appear><ListItem>2009 - Opposing parties meet in Oslo and achieve an agreement. ES3.1 is renamed to ES5</ListItem></Appear>
-                <Appear><ListItem>2015 - ES6 will be released.</ListItem></Appear>
-                <Appear><ListItem>Starting with ES6 - Version names will be based on the year of release. ES6 === ES2016 and ES7 === ES2016</ListItem></Appear>
+                <List>
+                  <Appear><ListItem>1996 - JavaScript is taken to standardization in ECMA.</ListItem></Appear>
+                  <Appear><ListItem>1997 - ECMA-262 (ECMAScript)</ListItem></Appear>
+                  <Appear><ListItem>1998 - ECMAScript 2</ListItem></Appear>
+                  <Appear><ListItem>1999 - ECMAScript 3</ListItem></Appear>
                 </List>
               </Fill>
             </Layout>
+          </Slide>
+          <Slide bgColor="#ffffff" transition={["zoom"]}>
+            <List>
+              <Appear><ListItem>2005 - Mozilla and Macromedia started Work on ECMAScript 4, but they renamed to ECMAScript 3.1</ListItem></Appear>
+              <Appear><ListItem>2009 - Opposing parties meet in Oslo and achieve an agreement. ES3.1 is renamed to ES5</ListItem></Appear>
+              <Appear><ListItem>2015 - ES6 will be released.</ListItem></Appear>
+              <Appear><ListItem>Starting with ES6 - Version names will be based on the year of release. ES6 === ES2016 and ES7 === ES2016</ListItem></Appear>
+            </List>
           </Slide>
           <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
             <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
@@ -111,7 +117,7 @@ export default class Presentation extends React.Component {
               Why I need use ES6?
             </Heading>
           </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
+          <Slide transition={["zoom", "fade"]} bgColor="#ffffff">
             <List>
             <Appear><ListItem>"Bad" Parts are fixed in ES6</ListItem></Appear>
             <Appear><ListItem>Syntax sugar</ListItem></Appear>
@@ -119,7 +125,7 @@ export default class Presentation extends React.Component {
             <Appear><ListItem>Async problem</ListItem></Appear>
             </List>
           </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
+          <Slide transition={["zoom", "fade"]} bgColor="#ffffff">
             <Heading size="3">Block Scope</Heading>
             <Heading size="5">ES5</Heading>
             <CodePane
@@ -128,24 +134,28 @@ export default class Presentation extends React.Component {
               margin="20px auto"
             />
           </Slide>
-          <Slide>
-            <Appear><Heading size="5">ES6</Heading></Appear>
+          <Slide bgColor="#ffffff" textColor="black" transition={["spin"]}>
+            <Appear><Heading size="5">ES6 Block</Heading></Appear>
             <Appear><Heading size="6">const and let is new var</Heading></Appear>
+            <Appear>
             <CodePane
               lang="js"
               source={require("raw!../assets/es6-block")}
               margin="20px auto"
             />
+            </Appear>
           </Slide>
-          <Slide>
+          <Slide bgColor="#ffffff" textColor="black" transition={["spin"]}>
             <Heading size="5">Long string problem</Heading>
+            <Appear>
             <CodePane
               lang="js"
               source={require("raw!../assets/es5-long-string")}
               margin="20px auto"
             />
+            </Appear>
           </Slide>
-          <Slide>
+          <Slide bgColor="#ffffff" textColor="black">
             <Heading size="5">ES6 String template and  interpolate</Heading>
             <Appear>
             <CodePane
@@ -162,122 +172,155 @@ export default class Presentation extends React.Component {
             />
             </Appear>
           </Slide>
-          <Slide>
-            <Heading size="5">ES6 Object declarations</Heading>
+          <Slide bgColor="#ffffff" textColor="black" transition={["spin"]}>
+            <Heading textColor="black" size="5">ES6 Object declarations</Heading>
+            <Appear>
             <CodePane
               lang="js"
               source={require("raw!../assets/es6-object-declaration")}
               margin="20px auto"
             />
+            </Appear>
           </Slide>
-          <Slide>
-            <Heading size="5">ES6: Default Params</Heading>
+          <Slide bgColor="#ffffff" textColor="black">
+            <Heading textColor="black" size="5">ES6: Default Params</Heading>
+            <Appear>
             <CodePane
               lang="js"
               source={require("raw!../assets/es6-default-params")}
               margin="20px auto"
             />
+            </Appear>
           </Slide>
-          <Slide>
-            <Heading size="5">ES5: functions</Heading>
+          <Slide bgColor="#ffffff" textColor="black" transition={["spin"]}>
+            <Heading textColor="black" size="5">ES5: functions</Heading>
+            <Appear>
             <CodePane
               lang="js"
               source={require("raw!../assets/es5-functions")}
               margin="20px auto"
             />
+            </Appear>
           </Slide>
-          <Slide>
-            <Heading size="5">ES6: Arrow functions</Heading>
+          <Slide bgColor="#ffffff" textColor="black">
+            <Heading textColor="black" size="5">ES6: Arrow functions</Heading>
+            <Appear>
             <CodePane
               lang="js"
               source={require("raw!../assets/es6-arrow-functions")}
               margin="20px auto"
             />
+            </Appear>
           </Slide>
-          <Slide>
-            <Heading size="5">ES6: Arrow functions this lexical</Heading>
+          <Slide bgColor="#ffffff" textColor="black" transition={["spin"]}>
+            <Heading textColor="black" size="5">ES6: Arrow functions this lexical</Heading>
+            <Appear>
+            <CodePane
+              lang="js"
+              source={require("raw!../assets/es6-arrow-functions-this-incorrect")}
+              margin="20px auto"
+            />
+            </Appear>
+            <Appear>
             <CodePane
               lang="js"
               source={require("raw!../assets/es6-arrow-functions-this")}
               margin="20px auto"
             />
+            </Appear>
           </Slide>
-          <Slide>
-            <Heading size="5">ES6: Classes</Heading>
+          <Slide bgColor="#ffffff" textColor="black">
+            <Heading textColor="black" size="5">ES6: Classes</Heading>
+            <Appear>
             <CodePane
               lang="js"
               source={require("raw!../assets/es6-classes")}
               margin="20px auto"
             />
+            </Appear>
           </Slide>
-          <Slide>
-            <Heading size="5">ES5: Async programming </Heading>
+          <Slide bgColor="#ffffff" textColor="black" transition={["spin"]}>
+            <Heading textColor="black" size="5">ES5: Async programming </Heading>
             <List>
-            <ListItem>Classic solution: callback</ListItem>
-            <ListItem>Problem: only one callback per async task</ListItem>
-            <ListItem>Problem: Nested functions create messy code</ListItem>
+            <Appear><ListItem>Classic solution: callback</ListItem></Appear>
+            <Appear><ListItem>Problem: only one callback per async task</ListItem></Appear>
+            <Appear><ListItem>Problem: Nested functions create messy code</ListItem></Appear>
             </List>
+            <Appear>
             <CodePane
               lang="jsx"
               source={require("raw!../assets/es5-callback-hell")}
               margin="20px auto"
             />
+            </Appear>
           </Slide>
-          <Slide>
-            <Heading size="5">ES6: Promise </Heading>
+          <Slide bgColor="#ffffff" textColor="black" transition={["spin"]}>
+            <Heading textColor="black" size="5">ES6: Promise </Heading>
             <List>
-            <ListItem>Object that keeps a result of async function(waiting, resolved, rejected</ListItem>
-            <ListItem>Allow to return promise object and with them</ListItem>
-            <ListItem>flattens nested promises to avoid "callback hell"</ListItem>
+            <Appear><ListItem>Object that keeps a result of async function(waiting, resolved, rejected</ListItem></Appear>
+            <Appear><ListItem>Allow to return promise object and with them</ListItem></Appear>
+            <Appear><ListItem>flattens nested promises to avoid "callback hell"</ListItem></Appear>
             </List>
+            <Appear>
             <CodePane
               lang="jsx"
               source={require("raw!../assets/es6-promise")}
               margin="20px auto"
             />
+            </Appear>
           </Slide>
-          <Slide>
-            <Heading size="5">ES5: Modules </Heading>
+          <Slide bgColor="#ffffff" textColor="black">
+            <Heading textColor="black" size="5">ES5: Modules </Heading>
             <List>
-            <ListItem>Immediately Invoked Function Expression</ListItem>
-            <ListItem>controls variable exposure</ListItem>
+            <Appear><ListItem>Immediately Invoked Function Expression</ListItem></Appear>
+            <Appear><ListItem>controls variable exposure</ListItem></Appear>
             </List>
+            <Appear>
             <CodePane
               lang="jsx"
               source={require("raw!../assets/es5-module")}
               margin="20px auto"
             />
+              </Appear>
           </Slide>
-          <Slide note="Need using tool suh as browserify, jspm, webpack">
-            <Heading size="5">ES6: Modules </Heading>
+          <Slide bgColor="#ffffff" textColor="black" note="Need using tool suh as browserify, jspm, webpack">
+            <Heading size="5" textColor="black">ES6: Modules </Heading>
             <CodePane
               lang="jsx"
               source={require("raw!../assets/es6-module")}
               margin="20px auto"
             />
           </Slide>
-          <Slide>
-            <Heading>Other ES6 features</Heading>
+          <Slide bgColor="#ffffff" textColor="black" transition={["spin"]}>
+            <Heading textColor="black">Other ES6 features</Heading>
               <List>
-              <ListItem>Proxy</ListItem>
-              <ListItem>Iterators</ListItem>
-              <ListItem>Generators</ListItem>
-              <ListItem>Symbols</ListItem>
-              <ListItem>Map/Set, WeakMap/WeakSet</ListItem>
-              <ListItem>extended standard library (Number, Math, Array)</ListItem>
+              <Appear><ListItem>Proxy</ListItem></Appear>
+              <Appear><ListItem>Iterators</ListItem></Appear>
+              <Appear><ListItem>Generators</ListItem></Appear>
+              <Appear><ListItem>Symbols</ListItem></Appear>
+              <Appear><ListItem>Map/Set, WeakMap/WeakSet</ListItem></Appear>
+              <Appear><ListItem>extended standard library (Number, Math, Array)</ListItem></Appear>
             </List>
           </Slide>
-          <Slide>
-            <Heading>ES6 Browser Support</Heading>
-            <Image width="600px" src={images.browserSupport.replace("/", "")} margin="40px 0px 0px 0px" />
+          <Slide bgColor="#ffffff" textColor="black">
+            <Heading textColor="black">ES6 Browser Support</Heading>
+            <Appear><Image width="600px" src={images.browserSupport.replace("/", "")} margin="40px 0px 0px 0px" /></Appear>
           </Slide>
-          <Slide>
-            <Heading>Babel</Heading>
+          <Slide bgColor="#ffffff" textColor="black">
+            <Heading textColor="black">Babel</Heading>
             <List>
-              <ListItem>transpile ES2015 code to es2015 code to ES6</ListItem>
-              <ListItem>transpile JSX(React) to JavaScript</ListItem>
-              <ListItem>transpile any of your cool libriry to ES6/7</ListItem>
+            <Appear><ListItem>transpile ES2015 code to es2015 code to ES6</ListItem></Appear>
+            <Appear><ListItem>transpile JSX(React) to JavaScript</ListItem></Appear>
+            <Appear><ListItem>transpile any of your cool libriry to ES6/7</ListItem></Appear>
             </List>
+            <Link href="https://github.com/nikolenkoanton92/babel-brown-bag">
+              <Text bold caps textColor="tertiary">Try to setup Babel</Text>
+            </Link>
+          </Slide>
+          <Slide bgColor="#ffffff" transition={["spin"]}>
+            <Heading size="4">Thank you :)</Heading>
+            <Image width="460px" src={images.finish.replace("/", "")} margin="40px 10px 0px 10px" />
+            <Image width="460px" src={images.callbacks.replace("/", "")} margin="40px 10px 0px 10px" />
           </Slide>
         </Deck>
       </Spectacle>
